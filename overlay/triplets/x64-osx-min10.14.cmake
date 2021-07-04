9a95d366df77e41d3408582f532509a0cd8a8873
Be <be@mixxx.org>
2021-07-04 08:49:35 -0500

    build Qt6 instead of Qt5

---
+++ overlay/triplets/x64-osx-min10.14.cmake
@@ -0,0 +1,19 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+
+# Mixxx loads fdk-aac dynamically at runtime. This allows the user to replace 
+# the version of fdk-aac we ship which has the patent-encumbered HE-AAC 
+#removed with another build that supports HE-AAC.
+if(${PORT} MATCHES "fdk-aac")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES x86_64)
+
+set(VCPKG_OSX_DEPLOYMENT_TARGET 10.14)
+set(VCPKG_C_FLAGS -mmacosx-version-min=10.14)
+set(VCPKG_CXX_FLAGS -mmacosx-version-min=10.14)
+
