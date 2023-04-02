a952de264a697b82a9a92eb484a3a469007eeb9b
Daniel Schürmann <daschuer@mixxx.org>
2023-04-02 13:14:55 +0200

    [portaudio] Update overlay to version v19.7.0 and add patches
    enable_jack_on_windows.patch to eneble JACK on windows
    pa_initalize_recursive_guard.patch to fix crash with FlexAsio

--- overlay/ports/portaudio/portfile.cmake
+++ overlay/ports/portaudio/portfile.cmake
@@ -1,34 +1,50 @@
-vcpkg_fail_port_install(ON_TARGET "uwp")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO Be-ing/portaudio
-    REF 0f7ab8fe8b4ab73d9c301385d67eee367d4f3dcc
-    SHA512 9f75f579349d776c93538f3aa8f4133d148da53029c73bc7635e2a2191709389e29d1f492ae6063b0ae768d4ae0b28738f6fddc65256d3eb1ac31fbba2f9eded
-    HEAD_REF cmake_rewrite
+    REPO PortAudio/portaudio
+    REF 147dd722548358763a8b649b3e4b41dfffbcfbb6
+    SHA512 0f56e5f5b004f51915f29771b8fc1fe886f1fef5d65ab5ea1db43f43c49917476b9eec14b36aa54d3e9fb4d8bdf61e68c79624d00b7e548d4c493395a758233a
+    PATCHES
+        fix-library-can-not-be-found.patch
+        enable_jack_on_windows.patch
+        pa_initalize_recursive_guard.patch
 )
 
+string(COMPARE EQUAL ${VCPKG_LIBRARY_LINKAGE} dynamic PA_BUILD_SHARED)
+string(COMPARE EQUAL ${VCPKG_LIBRARY_LINKAGE} static PA_BUILD_STATIC)
+
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    asio ASIO
+  FEATURES
+    asio PA_USE_ASIO
+    jack PA_USE_JACK
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS ${FEATURE_OPTIONS}
-    OPTIONS_DEBUG -DDEBUG_OUTPUT:BOOL=ON
-    PREFER_NINJA
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DPA_USE_DS=ON
+        -DPA_USE_WASAPI=ON
+        -DPA_USE_WDMKS=ON
+        -DPA_USE_WMME=ON
+        -DPA_LIBNAME_ADD_SUFFIX=OFF
+        -DPA_BUILD_SHARED=${PA_BUILD_SHARED}
+        -DPA_BUILD_STATIC=${PA_BUILD_STATIC}
+        -DPA_DLL_LINK_WITH_STATIC_RUNTIME=OFF
+    OPTIONS_DEBUG
+        -DPA_ENABLE_DEBUG_OUTPUT:BOOL=ON
 )
 
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/${PORT}")
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 endif()
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
